61f30c7d2424653ccc912a1b9211be0c1a3dc28e
Laine <34147581+LainePlayinGames@users.noreply.github.com>
2022-01-11 05:01:44 +0000

    fixed imaging so far

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const userName = 
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 io.on('connection', (socket) => {
+  console.log(socket.handshake.address);
+    var client_ip_address = socket.request.connection.remoteAddress;
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
 		console.log("-- " + msg);
