6167f5a2251a182c83f8afe35198dab00577fd72
Laine <34147581+LainePlayinGames@users.noreply.github.com>
2022-01-26 17:33:54 +0000

    Update 0.0.3

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-const userName = 
+var nickname; 
+
 
 
 app.get('/', (req, res) => {
@@ -12,19 +13,36 @@ io.on('connection', (socket) => {
   console.log(socket.handshake.address);
     var client_ip_address = socket.request.connection.remoteAddress;
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-		console.log("-- " + msg);
+    io.emit('chat message',socket.nickname + " >> " + msg);
+		console.log("");
+		console.log(socket.nickname + " " + msg);
+		console.log("ID: " + socket.id)
+		console.log("");
   });
 });
 io.on('connection', (socket) => {
 	console.log('User Joined');
-	socket.broadcast.emit('system message', "🔌USER JOINED🔌");
+	socket.broadcast.emit('system message', "🔌SOMONE JOINED🔌");
   socket.on('disconnect', () => {
 		console.log('User Left');
-    io.emit('system message', "🔌USER LEFT🔌");
+    io.emit('system message', "🔌" + socket.nickname + " LEFT🔌");
   });
 });
-
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+io.on('connection', function(socket) {
+	socket.on('send-nickname', function(nickname) {
+    socket.nickname = nickname;
+		console.log("    ");
+				console.log("    ");
+						console.log("    ");
+		console.log("Username: " + nickname);
+		console.log("Socket Username: " + socket.nickname);
+		console.log("Socket ID: " + socket.id);
+		console.log("Address: " + socket.handshake.address);
+				console.log("    ");
+						console.log("    ");
+								console.log("    ");
+});
 });
