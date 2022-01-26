ac28376f314af9bbbb9f244ecbe5336966f02bf2
Laine <34147581+LainePlayinGames@users.noreply.github.com>
2022-01-26 19:38:50 +0000

    0.0.5

--- index.js
+++ index.js
@@ -2,47 +2,60 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-var nickname; 
 
 
+var nickname;
+
+let messages = ['test', 'test2']
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-  console.log(socket.handshake.address);
-    var client_ip_address = socket.request.connection.remoteAddress;
-  socket.on('chat message', msg => {
-    io.emit('chat message',socket.nickname + " >> " + msg);
+	console.log(socket.handshake.address);
+	var client_ip_address = socket.request.connection.remoteAddress;
+	socket.on('chat message', msg => {
+		io.emit('chat message', socket.nickname + " >> " + msg);
+		let newLength = messages.push(socket.nickname + " >> " + msg);
 		console.log("");
 		console.log(socket.nickname + " " + msg);
 		console.log("ID: " + socket.id)
 		console.log("");
-  });
+	});
 });
 io.on('connection', (socket) => {
 	console.log('User Joined');
 	socket.broadcast.emit('system message', "🔌SOMONE JOINED🔌");
-  socket.on('disconnect', () => {
+
+
+	socket.on('disconnect', () => {
 		console.log('User Left');
-    io.emit('system message', "🔌" + socket.nickname + " LEFT🔌");
-  });
+		io.emit('system message', "🔌" + socket.nickname + " LEFT🔌");
+	});
 });
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+	console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-io.on('connection', function(socket) {
-	socket.on('send-nickname', function(nickname) {
-    socket.nickname = nickname;
+io.on('connection', function (socket) {
+	socket.on('send-nickname', function (nickname) {
+		if (nickname == "") {
+			// Send a new call to get a valid username.
+			return;
+		}
+		socket.nickname = nickname;
+
 		console.log("    ");
-				console.log("    ");
-						console.log("    ");
+		console.log("    ");
+		console.log("    ");
 		console.log("Username: " + nickname);
 		console.log("Socket Username: " + socket.nickname);
 		console.log("Socket ID: " + socket.id);
 		console.log("Address: " + socket.handshake.address);
-				console.log("    ");
-						console.log("    ");
-								console.log("    ");
-});
+		console.log("    ");
+		console.log("    ");
+		console.log("    ");
+
+
+	});
+
 });
