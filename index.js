d481732b6fcf3f29fe8416a2dccec8b56692e85f
Laine <34147581+LainePlayinGames@users.noreply.github.com>
2022-01-30 04:58:14 +0000

    0.0.7

--- index.js
+++ index.js
@@ -8,6 +8,48 @@ var nickname;
 
 let messages = ['test', 'test2']
 
+var timeText;
+var reportNumber = 1;
+var connectedUserCount = 0;
+var totalMessages = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var days = 0;
+
+var intervalID = setInterval(sendMessage, [1000]);
+
+function sendMessage() {
+  console.log("");
+	console.log("System check #" + reportNumber);
+	console.log("Connected Clients: " + connectedUserCount);
+	console.log("Total Chats Sent: " + totalMessages);
+	console.log("");
+	console.log(":UPTIME:" + days + ":" + hours + ":" + minutes + ":" + seconds);
+	console.log(seconds + " Seconds");
+	console.log(minutes + " Minutes");
+	console.log(hours + " Hours");
+	console.log(days + " Days");
+	io.emit('uptime',(days + ":" + hours + ":" + minutes + ":" + seconds) )
+	reportNumber++;
+	seconds++;
+	countUp();
+}
+function countUp() {
+  if(seconds >= 60){
+		seconds = 0;
+		minutes++;
+	}
+	if(minutes >= 60){
+		minutes = 0;
+		hours++;
+	}
+	if(hours >= 24){
+		hours = 0;
+		days++;
+	}
+}
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
@@ -21,15 +63,18 @@ io.on('connection', (socket) => {
 		console.log(socket.nickname + " " + msg);
 		console.log("ID: " + socket.id)
 		console.log("");
+		totalMessages++;
 	});
 });
 io.on('connection', (socket) => {
 	console.log('User Joined');
+	connectedUserCount++;
 	socket.broadcast.emit('system message', "🔌SOMONE JOINED🔌");
 
 
 	socket.on('disconnect', () => {
 		console.log('User Left');
+		connectedUserCount--;
 		io.emit('system message', "🔌" + socket.nickname + " LEFT🔌");
 	});
 });
